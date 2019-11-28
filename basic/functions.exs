78c8b92e5289b8674716823352633e3a791b246c
André Roberto Figueiró de Magalhães <arfddem@gmail.com>
2019-11-28 17:20:50 -0300

    dev: functions

---
+++ basic/functions.exs
@@ -0,0 +1,19 @@
+defmodule Printer do
+  def print(words) when is_list(words) do
+    words
+    |> Enum.each(fn(word) -> String.capitalize(word) end)
+    |> Enum.join(" - ")
+    |> print
+  end
+
+  def print(word) when is_binary(word) do
+    process_start() <> word <> process_end()
+  end
+
+  defp process_start, do: "Printed: -> { "
+  defp process_end, do: " }"
+end
+
+words_to_print = ["see", "how", "they", "line", "up", "?"]
+
+IO.puts(Printer.print(words_to_print))
